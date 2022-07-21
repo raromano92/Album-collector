10741044ddd0d3cda1a425e3e67543fb896552da
Robert Romano <bromano1992@gmail.com>
2022-07-20 22:48:51 -0400

    Added Tracklist model as a child to Album model

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+
+OPTIONS = (
+    ('Y', 'Yes'),
+    ('N', 'No')
+)
+
 class Album(models.Model):
     title = models.CharField(max_length=100)
     band = models.CharField(max_length=100)
@@ -14,3 +20,13 @@ class Album(models.Model):
     def get_absolute_url(self):
         return reverse("detail", kwargs={ "album_id": self.id })
     
+    
+class TrackList(models.Model):
+    track = models.CharField(max_length=100)
+    single = models.BooleanField(default=False, choices=OPTIONS)
+    order = models.IntegerField()
+    
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.get_track_display()} on {self.order}"
